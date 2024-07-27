df181bad10bf85557523ac28cd4ce5700ebbe84d
mosaber30 <mhd.hawk@gmail.com>
2024-07-27 08:19:39 +0200

    new update

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -28,43 +28,8 @@ form.addEventListener('click', async (event) => {
 
     } catch (error) {
         console.error('wrong password, maybe?');
+        // can i use     throw new Error('wrong username or password')? //
     }
 });
 
 
-
-/*
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert("hey you clicked");
-});
-
-
-
-const form = document.querySelector(".form--login");
-
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-   
-    const data = new FormData(form);
-    console.log(data);
-
-    const response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: data.get('email'),
-            password: data.get('password')
-        })
-    });
-});
-
-if(response.ok) {
-    const result = await response.json();
-    console.log(result);
-} else {
-    throw new Error('wrong username or password')
-}
-    */
